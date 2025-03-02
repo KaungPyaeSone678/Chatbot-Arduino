19932c0e1ed88a6dfe55af101a2b496aeef1dad8
Kaung Pyae Sone <kaungpyaesone@Kaungs-MacBook-Pro.local>
2025-03-02 20:35:15 +1100

    Clean the code by removing unnecessary comments

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,20 +5,14 @@ import { Send as SendIcon } from "@mui/icons-material";
 import SmartToyIcon from '@mui/icons-material/SmartToy';
 import PersonIcon from '@mui/icons-material/Person';
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import "@fontsource/poppins"; // This loads the default weight of 400
+import "@fontsource/poppins";
 import { CircularProgress } from '@mui/material';
-
-// import { motion } from "framer-motion";
-
-
-
 
 const theme = createTheme({
   typography: {
     fontFamily: "Poppins, Arial, sans-serif"
   }
 });
-
 
 
 const Chatbot = () => {
@@ -51,13 +45,13 @@ const Chatbot = () => {
       const botResponse = data.answer.replace(/\n/g, '<br />');
       setConversation([...conversation, { sender: 'user', text: message }, { sender: 'bot', text: botResponse }]);
       setLoading(false);
-      // setConversation([...conversation, { sender: 'user', text: message }, { sender: 'bot', text: data.answer }]);
     }
   };
 
+  // Send message when pressed "Enter"
   const handleKeyDown = (event) => {
     if (event.key === 'Enter' && !event.shiftKey) {
-      event.preventDefault();  // Prevents newline in the TextField
+      event.preventDefault();
       handleSendMessage();
     }
   };
@@ -67,65 +61,68 @@ const Chatbot = () => {
     <Grid container >
         
         <Grid size={12} sx={{backgroundColor: "#FAFAFA", height: "100vh"}}>
+          
           <Grid container
-          sx={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
           >
-            <Box
+            
+            <Box // Head Logo of the page
               sx={{
                 display: "flex",
                 justifyContent: "center",
                 alignItems: "center",
-                // border: "1px solid #000", // Border around the Box
-                borderRadius: "12px", // Rounded corners
-                padding: "0.7rem", // Padding to ensure space between border and content
-                width: "auto", // Automatically adjust width based on content
-                maxWidth: "200px", // Automatically adjust width based on content
+                borderRadius: "12px",
+                padding: "0.7rem",
+                width: "auto",
+                maxWidth: "200px",
                 marginTop: "2rem",
                 backgroundColor: "#F5F6F5",
                 boxShadow: 10,
                 zIndex: 10
               }}
             >
-            <Grid item xs={6}>
-              <Typography variant="h5"
-                sx={{
-                  // marginTop: "0.7rem",
-                  // marginLeft: "8rem",
-                  color: "#00CECB"
-                }}
-              >
-                Arduino
-              </Typography>
-            </Grid>
-            <Grid item xs={6}>
-              <Typography variant="h5"
-                sx={{
-                  // marginTop: "0.7rem",
-                  marginLeft: "0.7rem",
-                }}
-              >
-                Chatbot
-              </Typography>
-            </Grid>
+
+              <Grid item xs={6}>
+                <Typography variant="h5"
+                  sx={{
+                    color: "#00CECB"
+                  }}
+                >
+                  Arduino
+                </Typography>
+              </Grid>
+              <Grid item xs={6}>
+                <Typography variant="h5"
+                  sx={{
+                    marginLeft: "0.7rem",
+                  }}
+                >
+                  Chatbot
+                </Typography>
+              </Grid>
+
             </Box>
+
           </Grid>
-          <Box 
-          sx={{ 
-            maxHeight: '75vh', 
-            overflowY: 'auto', 
-            marginBottom: '5rem', 
-            marginX: {xs: '1rem', md: "4rem", lg: "4rem"}, 
-            marginTop: "2rem",
-            '&::-webkit-scrollbar': {
-              display: 'none',  // Hide scrollbar in Chrome, Safari
-            },
-            scrollbarWidth: 'none',
+
+          <Box // Chatbox to display messages
+            sx={{ 
+              maxHeight: '75vh', 
+              overflowY: 'auto', 
+              marginBottom: '5rem', 
+              marginX: {xs: '1rem', md: "4rem", lg: "4rem"}, 
+              marginTop: "2rem",
+              '&::-webkit-scrollbar': {
+                display: 'none',  // Hide scrollbar in Chrome, Safari
+              },
+              scrollbarWidth: 'none',
             }}>
             {conversation.length === 0 ? (
+
               <Typography variant="h3"
                 sx={{ color: "gray", textAlign: "center", marginTop: "3rem" }}
               >
@@ -136,38 +133,41 @@ const Chatbot = () => {
             conversation.map((msg, index) => (
             <>
               {msg.sender === 'user' ? (
+
                 <Typography
                   key={index}
                   align="right"
                   sx={{ marginY: "1rem", display: 'flex', justifyContent: 'flex-end' }}
                 >
                   <Box
-                  sx={{
-                    backgroundColor: "#00CECB", // Light gray background
-                    padding: "0.8rem 1.2rem", // Padding inside the bubble
-                    borderRadius: "1.5rem", // Rounded corners
-                    maxWidth: "60%", // Prevents the bubble from being too wide
-                    wordBreak: "break-word", // Ensures text wraps properly
-                  }}
+                    sx={{
+                      backgroundColor: "#00CECB", 
+                      padding: "0.8rem 1.2rem", 
+                      borderRadius: "1.5rem", 
+                      maxWidth: "60%", 
+                      wordBreak: "break-word", 
+                    }}  
                   >
                     {msg.text}
                   </Box>
                   <PersonIcon sx={{ marginX: "1rem",  marginTop: "0.7rem"}} />
                 </Typography>
+
               ) : (
+
                 <Typography
                   key={index}
                   align="left"
                   sx={{ marginY: "1rem", display: 'flex', justifyContent: 'flex-start' }}
                 >
                   <SmartToyIcon sx={{ marginX: "1rem"}} />
-                  {/* <span>{msg.text}</span> */}
                   <div
                     dangerouslySetInnerHTML={{
                       __html: msg.text, // This allows rendering HTML like <br />
                     }}
                   />
                 </Typography>
+
               )}
             </>
             ))
@@ -175,7 +175,8 @@ const Chatbot = () => {
             )}
             <div ref={messagesEndRef} />
           </Box>
-          <Grid
+
+          <Grid // Textfield to type in message and send
             container
             sx={{
               display: "flex",
@@ -186,81 +187,70 @@ const Chatbot = () => {
               width: "100vw",
               position: "fixed",
               bottom: 0
-              // bottom: 10,
-              // // marginLeft: "3rem",
-              // // marginRight: "3rem",
-              // maxWidth: "80rem",
-              // borderRadius: 50,
             }}
           >
-          <TextField
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-          onKeyDown={handleKeyDown}
-          label="Ask me anything about Arduino"
-          variant="outlined"
-          fullWidth
-          // margin="normal"
-          sx={{
-            // position: "fixed",
-            // bottom: 5,
-            // marginLeft: "3rem",
-            // marginRight: "3rem",
-            maxWidth: {
-              xs: "23rem",  // Phones
-              sm: "30rem",  // Tablets
-              md: "50rem",  // Small laptops
-              lg: "60rem",  // Desktops
-              xl: "70rem",  // Large screens
-            },
-            borderRadius: 50,
-            "& .MuiOutlinedInput-root": {
-              "& fieldset": {
-                borderColor: "black",  // Default border color
-              },
-              "&:hover fieldset": {
-                borderColor: "black",  // Border color on hover
-              },
-              "&.Mui-focused fieldset": {
-                borderColor: "#00CECB",  // Border color when focused
-              },
-            },
-            "& .MuiInputLabel-root": {
-              color: "black", // Default label color
-            },
-            "& .MuiInputLabel-root.Mui-focused": {
-              color: "#00CECB", // Label color when focused
-            },
-          }}
-          // InputLabelProps={{
-          //   style: {
-          //     marginX: "12px",  // You can also adjust label border radius if needed
-          //   },
-          // }}
-          InputProps={{
-            style: {
-              backgroundColor: "white",
-              marginX: "3rem",
-              borderRadius: "30px",  // Set border radius here
-            },
-            endAdornment: (
-              <IconButton
+
+            <TextField
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              onKeyDown={handleKeyDown}
+              label="Ask me anything about Arduino"
+              variant="outlined"
+              fullWidth
               sx={{
-                borderRadius: "50%", // To make the button circular
-                width: "36px", // Adjust the size of the button
-                height: "36px", // Adjust the size of the button
-                padding: 0,
-                marginRight: 1, // Space from the right 
+                maxWidth: {
+                  xs: "23rem",  
+                  sm: "30rem",  
+                  md: "50rem",  
+                  lg: "60rem",  
+                  xl: "70rem",  
+                },
+                borderRadius: 50,
+                "& .MuiOutlinedInput-root": {
+                  "& fieldset": {
+                    borderColor: "black",  // Default border color
+                  },
+                  "&:hover fieldset": {
+                    borderColor: "black",  // Border color on hover
+                  },
+                  "&.Mui-focused fieldset": {
+                    borderColor: "#00CECB",  // Border color when focused
+                  },
+                },
+                "& .MuiInputLabel-root": {
+                  color: "black", // Default label color
+                },
+                "& .MuiInputLabel-root.Mui-focused": {
+                  color: "#00CECB", // Label color when focused
+                },
               }}
-              >
-                {loading ? <CircularProgress size={24} sx={{ color: "#00CECB" }} /> : <SendIcon sx={{ color: "#00CECB" }} onClick={handleSendMessage} />}
-                {/* <SendIcon sx={{color: "#00CECB"}} onClick={handleSendMessage}/> */}
-              </IconButton>
-            ),
-          }}
-          />
+            
+            InputProps={{
+              style: {
+                backgroundColor: "white",
+                marginX: "3rem",
+                borderRadius: "30px", 
+              },
+              endAdornment: (
+                <IconButton
+                  sx={{
+                    borderRadius: "50%", 
+                    width: "36px", 
+                    height: "36px", 
+                    padding: 0,
+                    marginRight: 1, 
+                  }}
+                >
+                  {loading ? <CircularProgress size={24} sx={{ color: "#00CECB" }} /> : <SendIcon sx={{ color: "#00CECB" }} onClick={handleSendMessage} />}
+                </IconButton>
+              ),
+            }}
+            />
+
           </Grid>
+
         </Grid>
+
     </Grid>
     </ThemeProvider>
   );
